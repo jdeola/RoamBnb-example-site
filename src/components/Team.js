c74b46096407346a9970514bf88c9bbc346bbd6c
James Deola <jdeola1722@gmail.com>
2022-01-13 13:14:51 -0500

    fixed keyImg placeholder sizing breakpoints

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,47 +7,50 @@ function Team() {
 
 
     return (
-        <Container>
-            <h1 >Meet Our Team</h1>
-            
-            <Row>
-                <Col>
-                    <StaticImage 
-                        src="../images/jeremy.png"
-                        width={600}
-                        height={800}
-                    />
+        <>
+            <h1 className="text-center" >Meet Our Team</h1>
+            <Container className="d-flex justify-content-center align-items-center p-3">
+                
+                
+                <Row>
+                    <Col>
+                        <StaticImage 
+                            src="../images/jeremy.png"
+                            width={600}
+                            height={800}
+                        />
 
-                    <p>
-                        Jeremy has been in the real estate space for three years on the construction management side. He
-                        enjoys forming relationships with contractors and partners alike and making sure each project is carried
-                        out correctly. He loves helping people achieve their goals and strives for customer satisfaction. Jeremy is 
-                        a father to two and enjoys exercising, camping, skiing, boating, and dirt bikes.
-                    </p>
+                        <p>
+                            Jeremy has been in the real estate space for three years on the construction management side. He
+                            enjoys forming relationships with contractors and partners alike and making sure each project is carried
+                            out correctly. He loves helping people achieve their goals and strives for customer satisfaction. Jeremy is 
+                            a father to two and enjoys exercising, camping, skiing, boating, and dirt bikes.
+                        </p>
 
-                </Col>  
+                    </Col>  
 
-                <Col>
-                    <StaticImage 
-                        src="../images/kristen.png"
-                        width={600}
-                        height={800}
-                    />
+                    <Col>
+                        <StaticImage 
+                            src="../images/kristen.png"
+                            width={600}
+                            height={800}
+                        />
 
-                    <p>
-                        Kristen has been in the real estate and rental property space for over 10 years. She started out
-                        managing large apartment communities for a national management company. She has overseen many
-                        property management teams and dealt with apartment acquisitions, unit turnover, and major
-                        renovations. She is also a licensed real estate agent extremely detail oriented. Kristen is a mother to two
-                        and enjoys health and fitness, skiing, camping, and traveling. She also loves good food!
-                    </p>
+                        <p>
+                            Kristen has been in the real estate and rental property space for over 10 years. She started out
+                            managing large apartment communities for a national management company. She has overseen many
+                            property management teams and dealt with apartment acquisitions, unit turnover, and major
+                            renovations. She is also a licensed real estate agent extremely detail oriented. Kristen is a mother to two
+                            and enjoys health and fitness, skiing, camping, and traveling. She also loves good food!
+                        </p>
 
-                </Col>  
-            </Row>
+                    </Col>  
+                </Row>
 
 
 
-        </Container>
+            </Container>
+        </>
     )
 }
 
